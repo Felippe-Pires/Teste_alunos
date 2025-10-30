707392ae5aa93a8878c667f2ab78732a8afa365f
wilian-sysinfo <wilian.alunosenac@gmail.com>
2025-10-29 21:26:33 -0300

    subtracao luciano

--- soma_subtracao.py
+++ soma_subtracao.py
@@ -4,3 +4,11 @@ def soma(a, b):
 # Exemplo de chamada da função
 resultado = soma(5, 3)
 print(resultado)  # Saída: 8
+
+# SUBTRAÇÃO 
+def subtracao(a, b):
+    return a - b
+
+# Exemplo de chamada da função
+resultado = subtracao(5, 3)
+print(resultado)  # Saída: 2
