556b25d66fb1bfac0c5e8f184537695a0f931ff7
yvesk.martins@gmail.com <yves59739656@edu.df.senac.br>
2025-10-29 21:24:01 -0300

    correção da função cossecante

--- cossecante.py
+++ cossecante.py
@@ -1,9 +1,10 @@
 from math import sin
 
-def secante(valor):
-    if cos(valor) == 0:
+
+def cossecante(valor):
+    if sin(valor) == 0:
         raise ValueError(
-            "O ângulo resulta em uma secante indefinida (divisão por zero)."
+            "O ângulo resulta em uma cossecante indefinida (divisão por zero)."
         )
     return 1 / sin(valor)
 
@@ -11,7 +12,7 @@ def secante(valor):
 if __name__ == "__main__":
     angulo = float(input("Digite o ângulo em radianos: "))
     try:
-        resultado = secante(angulo)
-        print(f"A secante de {angulo} radianos é: {resultado}")
+        resultado = cossecante(angulo)
+        print(f"A cossecante de {angulo} radianos é: {resultado}")
     except ValueError as e:
         print(e)
