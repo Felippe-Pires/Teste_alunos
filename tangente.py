4bdb67e4f1d4dc341230501d94e367625e07b36c
thaistavares-cell <comercialmarinho974@gmail.com>
2025-10-29 21:53:03 -0300

    calculo da tangente

---
+++ tangente.py
@@ -0,0 +1,7 @@
+# função para calcular a tangente de um angulo
+def calcular_tangente(angulo):
+    import math
+    angulo_radiano = math.radians(angulo)
+    tangente = math.tan(angulo_radiano)
+    return tangente
+
